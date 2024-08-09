5c93d5db7fdba2db04962c6ba92a597d9ac624b2
Masiori <juanfegolo@gmail.com>
2024-08-08 21:39:22 -0500

    Matenme

---
+++ src/components/Hobbies.tsx
@@ -0,0 +1,32 @@
+export const Hobbies = () => {
+    return(
+        <>
+        
+        <div>
+        
+            <h1 className="text-3xl font-bold text-black">Hobbies</h1>
+            
+            <div className= "w-11/12">
+            <p className=" font-mono">
+            en mis tiempos libres cuando no estoy estudiando o trabajando en algun proyecto me dedico principalmente a jugar videojuegos, usualmente juegos multijugador 
+            aunque tambien disfruto mucho de juegos solitarios. Jugar videojuegos ha sido una fuente invaluable de crecimiento tanto personal como profesional para mí. A través de los videojuegos,
+             he desarrollado habilidades clave como la resolución de problemas, el pensamiento estratégico, y la capacidad para tomar decisiones rápidas bajo presión. 
+            Además, la naturaleza colaborativa de muchos juegos me ha enseñado a trabajar en equipo y a comunicarme de manera efectiva, cualidades que aplico diariamente en mi vida profesional. 
+            Esta experiencia lúdica también me ha permitido explorar nuevas formas de creatividad y adaptabilidad, 
+            fortaleciendo mi capacidad para enfrentar desafíos de manera innovadora. 
+            <img src="https://cdn.mobygames.com/covers/7176551-dying-light-playstation-4-front-cover.jpg" alt = "Miyuki" className = "w-64 h-64"/>
+            </p>
+            <br/>
+            <p className="font-mono">otro pasatiempo es el miyuki ya que 
+            Practicar miyuki, o la creación de joyería con cuentas, ha sido una actividad profundamente enriquecedora para mi desarrollo personal y profesional. A través de esta práctica, he cultivado 
+            una gran paciencia y atención al detalle, habilidades esenciales para lograr precisión en cualquier tarea. Además, el proceso creativo de diseñar patrones y combinaciones de colores me ha 
+            permitido mejorar mi capacidad para innovar y pensar de manera estética, lo cual aplico en diversas áreas de mi vida. Esta actividad también me enseña a manejar el tiempo de manera eficiente y a 
+            perseverar en la consecución de metas, cualidades que son fundamentales en mi desempeño profesional.</p>
+            <img src="src\images\Miyuki.jpeg" alt = "Miyuki" className = "w-64 h-64"/>
+            </div>
+
+
+        </div>
+        </>
+    )
+}
